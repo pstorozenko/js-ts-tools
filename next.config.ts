26d39b094eae5f149a281f44bc4af7ef868ad751
Piotr Pasza Storozenko <piotr@appsilon.com>
2024-11-05 15:14:08 +0100

    gh pages workflow

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  /* config options here */
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
